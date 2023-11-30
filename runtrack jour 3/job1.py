366cae533a01ae00f3e24a91df927d159d208830
djibrilzeroual <115222869+djibrilzeroual@users.noreply.github.com>
2023-11-30 14:13:41 +0100

    Add files via upload

---
+++ runtrack jour 3/job1.py
@@ -0,0 +1,7 @@
+def my_print_hello():
+    print("hello wold")
+
+my_print_hello()
+
+
+
---
+++ runtrack jour 3/job2.py
@@ -0,0 +1,7 @@
+def my_print_name(name):
+    print(name)
+
+
+my_print_name("benjamin")
+my_print_name("mendy")
+my_print_name("chef")
